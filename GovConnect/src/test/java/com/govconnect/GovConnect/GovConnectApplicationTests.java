9b4b5ff92b894239739973f2eebcfc1ff4a0d412
KaroK365 <145526777+KaroK365@users.noreply.github.com>
2024-10-25 23:42:15 +0530

    Add files via upload

---
+++ GovConnect/src/test/java/com/govconnect/GovConnect/GovConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govconnect.GovConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
