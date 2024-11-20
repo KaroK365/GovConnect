6b668d27de46e55561c0d52a6c0b7bc3530f63bb
Karok365 <anshsinghh365@gmail.com>
2024-11-20 16:16:12 +0530

    User and Matter class done

--- GovConnect/src/main/java/com/govconnect/GovConnect/Entity/Matter.java
+++ GovConnect/src/main/java/com/govconnect/GovConnect/Entity/Matter.java
@@ -1,4 +1,19 @@
 package com.govconnect.GovConnect.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Matter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long likes;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- GovConnect/src/main/java/com/govconnect/GovConnect/Entity/User.java
+++ GovConnect/src/main/java/com/govconnect/GovConnect/Entity/User.java
@@ -1,19 +1,27 @@
 package com.govconnect.GovConnect.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer Id;
-    String Usernmame;
-    String Password;
+    private Long Id;
 
+    @Column(nullable = false, unique = true)
+    private String username;  // Fixed typo in field name
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Matter> posts = new ArrayList<>();
 }
