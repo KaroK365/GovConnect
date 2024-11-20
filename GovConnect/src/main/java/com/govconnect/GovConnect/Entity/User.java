94c2478ae49aaad5359cb39bcac2163187345f56
Karok365 <anshsinghh365@gmail.com>
2024-11-20 16:05:35 +0530

    User Class created

--- GovConnect/src/main/java/com/govconnect/GovConnect/Entity/User.java
+++ GovConnect/src/main/java/com/govconnect/GovConnect/Entity/User.java
@@ -1,4 +1,19 @@
 package com.govconnect.GovConnect.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer Id;
+    String Usernmame;
+    String Password;
+
 }
