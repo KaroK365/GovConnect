e99652fb9f349d116f2fe063a430a7f8d0a5cae7
Karok365 <anshsinghh365@gmail.com>
2024-11-25 01:17:54 +0530

    User Service written (needs error handling)

--- GovConnect/src/main/java/com/govconnect/GovConnect/Service/UserService.java
+++ GovConnect/src/main/java/com/govconnect/GovConnect/Service/UserService.java
@@ -1,4 +1,28 @@
 package com.govconnect.GovConnect.Service;
 
+import com.govconnect.GovConnect.Entity.User;
+import com.govconnect.GovConnect.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public Optional<User> getUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void DeleteUser(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+        user.ifPresent(value -> userRepository.delete(value));
+    }
 }
